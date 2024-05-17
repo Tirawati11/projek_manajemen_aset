a8ec37ed4caf5a80f34c0b6842d3e099372ae6f7
LAPTOP-1OTEMH15\LENOVO <kdalenti@gmail.com>
2024-05-17 10:07:14 +0700

    create

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,9 +1,10 @@
+<!DOCTYPE html>
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
     <h1>Create New Category</h1>
-    <form action="{{ route('categories.store') }}" method="POST">
+    <form action="{{ route('category.store') }}" method="POST">
         @csrf
         <div class="form-group">
             <label for="name">Category Name</label>
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,34 +1,48 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1>Categories</h1>
-    <a href="{{ route('categories.create') }}" class="btn btn-primary">Create Category</a>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($categories as $category)
-                <tr>
-                    <td>{{ $category->name }}</td>
-                    <td>{{ $category->description }}</td>
-                    <td>
-                        <a href="{{ route('categories.show', $category->id) }}" class="btn btn-info">View</a>
-                        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-warning">Edit</a>
-                        <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-@endsection
+ <!DOCTYPE html>
+    @extends('layouts.main')
+                    
+     @section('container')
+                    
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Kategori - Kategori</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    </head>
+    <body style="background: lightgray">
+                    
+        <div class="container">
+            <h1>Categories</h1>
+            <a href="{{ route('categories.create') }}" class="btn btn-primary">Create Category</a>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">NO</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($categories as $category)
+                        <tr>
+                            <td>{{ $key+1 }}</td>
+                            <td>{{ $category->name }}</td>
+                            <td>{{ $category->description }}</td>
+                            <td>
+                                <a href="{{ route('categories.show', $category->id) }}" class="btn btn-info">View</a>
+                                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-warning">Edit</a>
+                                <form action="{{ route('categories.destroy', $category->id) }}" method="POST" style="display:inline-block;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        @endsection
